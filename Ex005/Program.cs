576f84764d87da4e12d6ab15c0dd950b676fd057
Ekaterina <katerina-kozyrenko@yandex.ru>
2022-08-04 13:14:47 +0300

    ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "катя")
+{
+    Console.WriteLine("Да здавствует Катя!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
