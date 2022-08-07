220ae055b03cfe6a2a9c08286f94bf864674ee28
Ekaterina <katerina-kozyrenko@yandex.ru>
2022-08-07 09:15:34 +0300

    add_ex009

---
+++ Ex009/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine());
+int sqr = numB * numB;
+if (numA == sqr)
+Console.Write($"{numB} является квадратом числа {numA}");
+else
+Console.Write($"{numB} не является квадратом числа {numA}");
