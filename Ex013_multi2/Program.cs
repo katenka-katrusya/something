62383b8f5b905e8616700c1dc72a4024be5eb52b
SEkaterina <katerina-kozyrenko@yandex.ru>
2022-08-11 20:59:30 +0300

    add_ex013

---
+++ Ex013_multi2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. Напр, 14 -> нет, 161 -> да
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
