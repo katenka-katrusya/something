d52a2b2684d540cabdbfb417f9561465094a5d03
SEkaterina <katerina-kozyrenko@yandex.ru>
2022-08-13 23:24:23 +0300

    add_ex017

---
+++ Ex017_2D/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
+
+Console.Write("x1 = ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("y1 = ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("x2 = ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("y2 = ");
+int y2 = int.Parse(Console.ReadLine());
+
+double s = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 -y1, 2));
+Console.WriteLine($"Расстояние между двумя точками = {s}");
