01fd2b325ec280ab89af774916929373dbef8e72
SEkaterina <katerina-kozyrenko@yandex.ru>
2022-08-15 14:54:23 +0300

    ex020_for

---
+++ Ex020_for/Program.cs
@@ -0,0 +1,12 @@
+﻿// Цикл for
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
