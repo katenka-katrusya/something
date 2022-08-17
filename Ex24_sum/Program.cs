cd8db5041cb49a533bfcfc5a5e6b4ff00bde45da
SEkaterina <katerina-kozyrenko@yandex.ru>
2022-08-17 14:13:26 +0300

    upd

---
+++ Ex24_sum/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, кот. принимает на вход число (А) и выдает сумму чисел от 1 до А. Напр., 7 -> 28 
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetSumNums(int num)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i; // sum = sum + i
+    }
+    return sum;
+}
+Console.WriteLine(GetSumNums(num));
---
+++ Ex27_array/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, кот. выводит массив из 8 элементов, заполненный 0 и 1 в случайном порядке
+
+void RandArray()
+{
+    int[] arr = new int[10];
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = new Random().Next(0, 2);
+            //Console.Write("{0} ", arr[i]);
+        }
+    }
+        
+    Console.WriteLine($"[{String.Join(", ", arr)}]");
+}
+
+RandArray();
