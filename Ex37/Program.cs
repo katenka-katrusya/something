22947181b9184c3b34af54cdf334115208d6d5a3
SEkaterina <katerina-kozyrenko@yandex.ru>
2022-08-20 17:44:45 +0300

    ex37

---
+++ Ex37/Program.cs
@@ -0,0 +1,30 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпослед. и тд
+
+int[] GetArray(int size) 
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(1, 100);
+    }
+    return res;                                      
+}
+
+int[] ResultArray(int[] array)
+{
+    int size = array.Length / 2;
+    if (array.Length % 2 == 1) size++;
+    int[] result = new int[size];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length - 1 - i];
+    }
+    if (array.Length % 2 == 1) result[size-1] = array[array.Length / 2];
+    return result;
+}  
+
+int[] array = GetArray(5);
+int[] GroupArr = ResultArray(array);
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine(String.Join(" ", GroupArr));
