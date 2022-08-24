f04f0d7c0382eb43d7bfe67522a93d5ced7dea29
SEkaterina <katerina-kozyrenko@yandex.ru>
2022-08-24 13:49:39 +0300

    task42

---
+++ Ex42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42. Напишите программу, кот. будет преобразовывать десятичное число в двоичное
+
+Console.Write("Введите десятичное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetBinary(int number)
+{
+    string result = "";
+    
+    while (number > 0)
+    {
+        result += Convert.ToString(number % 2);
+        number /= 2;
+    }
+    result.ToCharArray().Reverse();
+    
+    return int.Parse(result);
+}
+
+Console.Write($"{GetBinary(num)}");
